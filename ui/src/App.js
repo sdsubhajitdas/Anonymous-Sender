b0370cd4562bc90c0289e6ee94754996836a9469
Subhajit Das <sdsubhajitdas98@gmail.com>
2022-08-24 21:02:24 +0530

    Login page implemented

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,8 +1,84 @@
-import { Container, TextField } from "@mui/material";
 import Login from "./pages/Login/index";
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
-function App() {
-  return <div></div>;
+export default function App() {
+  const theme = createTheme(getDesignTokens("dark"));
+  // const theme = createTheme(getDesignTokens("light"));
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          border: 15,
+          borderColor: "primary.main",
+          width: "100%",
+          height: "100%",
+          minHeight: "100vh",
+          padding: 1,
+        }}
+      >
+        <CssBaseline />
+        <Login />
+      </Box>
+    </ThemeProvider>
+  );
 }
 
-export default App;
+function getDesignTokens(mode) {
+  return {
+    palette: {
+      mode,
+      background: {
+        ...(mode === "dark" && {
+          paper: "#424242",
+          default: "#303030",
+        }),
+        ...(mode === "light" && {
+          paper: "#FFFFFF",
+          default: "#FAFAFA",
+        }),
+      },
+      divider:
+        mode === "dark" ? "rgba(255, 255, 255, 0.12)" : "rgba(0, 0, 0, 0.12)",
+      primary: {
+        main: "#7B16FF",
+        light: "#9544FF",
+        dark: "#560FB2",
+      },
+      secondary: {
+        main: "#F50057",
+        light: "#F73378",
+        dark: "#AB003C",
+      },
+      error: {
+        main: "#F44336",
+        light: "#E57373",
+        dark: "#D32F2F",
+      },
+      warning: {
+        main: "#FF9800",
+        light: "#FFB74D",
+        dark: "#F57C00",
+      },
+      info: {
+        main: "#2196F3",
+        light: "#64B5F6",
+        dark: "#1976D2",
+      },
+      success: {
+        main: "#4CAF50",
+        light: "#81C784",
+        dark: "#388E3C",
+      },
+    },
+    typography: {
+      logoLarge: {
+        fontFamily: ["Gochi Hand", "cursive"].join(","),
+        fontWeight: 400,
+        fontSize: "4.5rem",
+        lineHeight: 1.18,
+        letterSpacing: "−0.01198em",
+      },
+    },
+  };
+}
---
+++ ui/src/pages/Login/index.js
@@ -0,0 +1,162 @@
+import {
+  Box,
+  TextField,
+  Typography,
+  useTheme,
+  Stack,
+  Button,
+  Alert,
+  AlertTitle,
+  Collapse,
+  LinearProgress,
+} from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
+import Logo from "../../components/Logo/index";
+
+axios.defaults.baseURL = process.env.REACT_APP_API_ENDPOINT_URL;
+
+function Login() {
+  const theme = useTheme();
+  const logoStyle = {
+    fill: theme.palette.text.primary,
+    height: parseInt(theme.spacing()) * 13,
+    padding: parseInt(theme.spacing()),
+    width: parseInt(theme.spacing()) * 13,
+  };
+  const loginStyle = {
+    backgroundColor: theme.palette.background.paper,
+    borderRadius: 3,
+    boxShadow: theme.shadows[15],
+    margin: "auto",
+    marginY: 9,
+    maxWidth: 400,
+    p: 3,
+  };
+
+  async function login(event) {
+    event.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axios.post("/users/login", {
+        email: formValues.email,
+        password: formValues.password,
+      });
+      console.log(response.data);
+      setError(null);
+      setErrorAlertOpen(false);
+      setSuccessAlertOpen(true);
+    } catch (error) {
+      setError(error.response.data);
+      setErrorAlertOpen(true);
+      setSuccessAlertOpen(false);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  let [formValues, setFormValues] = useState({ email: "", password: "" });
+  let [errorAlertOpen, setErrorAlertOpen] = useState(false);
+  let [successAlertOpen, setSuccessAlertOpen] = useState(false);
+  let [error, setError] = useState(null);
+  let [loading, setLoading] = useState(false);
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: {
+            xs: "column",
+            sm: "row",
+          },
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Logo style={logoStyle} />
+        <Typography
+          variant="logoLarge"
+          align="center"
+          component={"h2"}
+          sx={{
+            ml: {
+              xs: 0,
+              sm: 2.5,
+              md: 5,
+            },
+          }}
+        >
+          Anonymous Sender
+        </Typography>
+      </Box>
+      {error && (
+        <Collapse in={errorAlertOpen} sx={{ mt: 3, mx: [2, 15, 30, 45, 60] }}>
+          <Alert
+            severity="error"
+            variant="filled"
+            onClose={() => {
+              setErrorAlertOpen(false);
+            }}
+          >
+            <AlertTitle>Login Error</AlertTitle>
+            {error}
+          </Alert>
+        </Collapse>
+      )}
+      {!error && (
+        <Collapse in={successAlertOpen} sx={{ mt: 3, mx: [2, 15, 30, 45, 60] }}>
+          <Alert
+            severity="success"
+            variant="filled"
+            onClose={() => {
+              setSuccessAlertOpen(false);
+            }}
+          >
+            <AlertTitle>Login Error</AlertTitle>
+            Login successful. Redirect function not yet implemented !! :(
+          </Alert>
+        </Collapse>
+      )}
+      <form>
+        <Stack spacing={2} sx={loginStyle}>
+          <Typography variant="h3" align="center">
+            Login
+          </Typography>
+          <TextField
+            label="Email"
+            color="secondary"
+            type={"email"}
+            value={formValues.email}
+            onChange={(e) =>
+              setFormValues({ ...formValues, email: e.target.value })
+            }
+            disabled={loading}
+          />
+          <TextField
+            label="Password"
+            color="secondary"
+            type={"password"}
+            value={formValues.password}
+            onChange={(e) =>
+              setFormValues({ ...formValues, password: e.target.value })
+            }
+            disabled={loading}
+          />
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={login}
+            type="submit"
+            disabled={loading}
+          >
+            Login
+          </Button>
+          {loading && <LinearProgress color="warning" />}
+        </Stack>
+      </form>
+    </>
+  );
+}
+
+export default Login;
